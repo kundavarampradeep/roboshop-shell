70721cbc1e60a40391eafab2447db6d0d38b4886
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-04-12 13:13:34 +0530

    added catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -29,13 +29,18 @@ VALIDATE(){
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copied MongoDB repo into yum.repos.d"
+
 yum install mongodb-org -y &>> $LOGFILE
 VALIDATE $? "Installationf of MongoDB"
+
 systemctl enable mongod  &>> $LOGFILE
 VALIDATE $? "Enabling MongoDB"
+
 systemctl start mongod  &>> $LOGFILE
 VALIDATE $? "starting MongoDB"
+
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 VALIDATE $? "Edited MongoDB conf"
+
 systemctl restart mongod &>> $LOGFILE
 VALIDATE $? "Restarting MongoDB"
